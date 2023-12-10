eed4c32616c9c70dce65bb331996a2eec41f7044
Kenneth <kennethw@bbd.co.za>
2023-12-10 13:58:32 +0200

    Day 4 - Part 1 Done

--- day4/main.py
+++ day4/main.py
@@ -1,4 +1,24 @@
 data = open('input.txt')
 
+total_points = 0
+
 for line in data:
+    win_count = 0
+
     row = line.rstrip()
+
+    win_nums, nums = row.split(' | ')
+
+    card, win_nums = win_nums.split(': ')
+
+    win_nums = win_nums.split()
+    nums = nums.split()
+    
+    for i in win_nums:
+        if i in nums:
+            win_count += 1
+
+    if win_count > 0:
+        total_points += 2**(win_count-1)
+
+print("Total Points : ", total_points)
